5e0fc2d216037bb7d8c787c2ae899c7efebc1b65
haku <hakumatsunoki@gmail.com>
2024-10-25 20:39:49 +0300

    add databases, add docker-compose.yml

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,3 @@ RUN python -m pip install -r requirements.txt
 COPY . .
 
 EXPOSE 8000
-
-RUN chmod +x ./start.sh
-
-CMD ["./start.sh"]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,22 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.conf import settings
+import redis
 
 
 # Create your views here.
 class CheckHealthView(APIView):
+    cache = redis.StrictRedis(
+        host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True, db=0
+    )
+
     def get(self, request):
-        return Response({"status_code": 200, "detail": "ok", "result": "working"})
+        cached_result = self.cache.hgetall("json_key")
+
+        if cached_result:
+            return Response(cached_result)
+
+        result = {"status_code": 200, "detail": "ok", "result": "working"}
+        self.cache.hmset("json_key", result)
+
+        return Response(result)
