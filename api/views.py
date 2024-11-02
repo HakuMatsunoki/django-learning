5a40135efcfc1afcf0b2343e7817fc06022a4c41
haku <hakumatsunoki@gmail.com>
2024-11-02 15:06:27 +0200

    add timestamp abstract model, migrations, logging to the file

--- api/views.py
+++ api/views.py
@@ -1,17 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.core.cache import cache
+import logging
 
 
 # Create your views here.
 class CheckHealthView(APIView):
+    logger = logging.getLogger("django")
+
     def get(self, request):
         cached_result = cache.get("json_key")
 
         if cached_result:
+            self.logger.info("cache used")
+
             return Response(cached_result)
 
         result = {"status_code": 200, "detail": "ok", "result": "working"}
         cache.set("json_key", result, 30)
+        self.logger.info("fresh response")
 
         return Response(result)
