26d74f009c393f88adfae159dc0161b0ed314a24
haku <hakumatsunoki@gmail.com>
2024-11-08 09:38:49 +0200

    rm start script migrations, rename githib workflow

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo ">>> make migrations >>>"
-python manage.py makemigrations
-
-echo ">>> migrate >>>"
-python manage.py migrate
-
 echo ">>> run server >>>"
 exec python manage.py runserver 0.0.0.0:8000
